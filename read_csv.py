3f82b1a21d4d9d17386035e8c0abab6854069849
Zhou Hui <tina.zhou.hui@gmail.com>
2020-05-01 19:46:59 +0200

    list of months available in disctionary..almost

--- read_csv.py
+++ read_csv.py
@@ -12,6 +12,7 @@ def read_csv_create_dict(path):
     """
     with open(path, newline='') as csvfile:
         input_data = csv.reader(csvfile, delimiter=";")
+        month_year = []
         output = []
         next(csvfile) #skips header row
         for row in input_data:
@@ -23,6 +24,12 @@ def read_csv_create_dict(path):
                 'Date': datetime.strptime(row[4][3:], '%m.%Y').strftime("%B %Y")
             }
             output.append(body_dict)
-        return output
+
+            print(type(body_dict['Date'])) #this is a string
+            #create a list of all months and years available in dictionary
+            if body_dict['Date'] not in month_year:
+                month_year = month_year.append(body_dict['Date'])
+
+        return output, month_year
 
 print(read_csv_create_dict('./inputs/data_dec19.csv'))
