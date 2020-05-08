8ed1e71340e5f46111fcba0ba0ff7173631f8b5a
Zhou Hui <tina.zhou.hui@gmail.com>
2020-05-08 16:33:38 +0200

    + add unit test that checks article columns

--- tests/input_prep.py
+++ tests/input_prep.py
@@ -1,6 +1,6 @@
 import unittest
 import csv
-from app.input_prep import articles, pages
+from app.input_prep import read_csv, articles, pages
 from datetime import datetime
 
 class TestReadCsv(unittest.TestCase):
@@ -11,6 +11,16 @@ class TestReadCsv(unittest.TestCase):
     def test_articles_empty(self):
         output = articles([])
         self.assertEqual(output, [], 'The output is not empty list')
+
+    def test_articles_columns(self):
+        input = [['col1', 'col2', 'col3', 'col4', '24.12.2020', 'col6']]
+        output = articles(input)
+        self.assertEqual(output[0]['title'], "col1")
+        self.assertEqual(output[0]['link'], "col2")
+        self.assertEqual(output[0]['source'], "col3")
+        self.assertEqual(output[0]['summary'], "col4")
+        self.assertEqual(output[0]['date'], "December 2020")
+        self.assertEqual(len(output[0]), 5)
 
     def test_pages_empty(self):
         output = pages({})
