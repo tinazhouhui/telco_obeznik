44f9119a85160c6bb4d3b26aaa6cce165d9772a5
Zhou Hui <tina.zhou.hui@gmail.com>
2020-05-08 14:28:04 +0200

    get unittest to run

--- generate.py
+++ generate.py
@@ -1,7 +1,7 @@
 # read csv file
 # replace value in template
 
-from read_csv import read_csv, articles, pages
+from app.input_prep import read_csv, articles, pages
 
 CSV_FILE = read_csv('./inputs/data_dec19.csv')
 ARTICLES = articles(CSV_FILE)
