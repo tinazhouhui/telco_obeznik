606090f2f39cfdb87b4449af4ee489318fae7316
Zhou Hui <tina.zhou.hui@gmail.com>
2020-05-10 19:21:07 +0200

    C:/Users/tzhouhui/AppData/Local/Programs/Git/ easier function pipeline generation

--- generate.py
+++ generate.py
@@ -1,8 +1,17 @@
 """
 Generator of all functions.
 """
-from app.input_prep import read_csv, articles, pages
+from app.input_prep import validate_data, read_csv, articles, pages
 
-CSV_FILE = read_csv('./inputs/data_dec19.csv')
-ARTICLES = articles(CSV_FILE)
-print(pages(ARTICLES))
+#CSV_FILE = read_csv('./inputs/data_dec19.csv')
+#VALIDATED_DATA = validate_data(CSV_FILE)
+#ARTICLES = articles(VALIDATED_DATA)
+#print(pages(ARTICLES))
+
+PIPELINE = [read_csv, validate_data, articles, pages]
+OUTPUT = './inputs/data_dec19.csv'
+
+for function in PIPELINE:
+    OUTPUT = function(OUTPUT)
+
+print(OUTPUT)
