d692d8e466f7add59cfeedd1888a7c419edebf8b
Zhou Hui <tina.zhou.hui@gmail.com>
2020-05-20 09:28:20 +0200

    + separate world and czech dict and merged to one, implemented in templates

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@
 Generator of all functions.
 """
 
-from collections import defaultdict
+import pprint
 from app.input_prep import validate_data, read_csv, articles, pages
 from app.article_list_render import ArticleListRender
 
@@ -14,19 +14,30 @@ from app.article_list_render import ArticleListRender
 PIPELINE = [read_csv, validate_data, articles, pages]
 OUTPUT_CZECH = './inputs/data_dec19.csv'
 OUTPUT_WORLD = './inputs/data_world.csv'
-# OUTPUT_WORLD =
 
 for function in PIPELINE:
     OUTPUT_CZECH = function(OUTPUT_CZECH)
     OUTPUT_WORLD = function(OUTPUT_WORLD)
 
-OUTPUT = defaultdict(list)
+OUTPUT = {}
 
-for page in (OUTPUT_CZECH, OUTPUT_WORLD):
-    for key, value in page.items():
-        OUTPUT[key].append(value)
+for page_world in OUTPUT_WORLD:
+    OUTPUT[page_world] = {
+        'world': OUTPUT_WORLD[page_world],
+        'czech': [],
+    }
 
-# print(OUTPUT)
+for page_czech in OUTPUT_CZECH:
+    if page_czech not in OUTPUT:
+        OUTPUT[page_czech] = {
+            'world': [],
+            'czech': [],
+        }
+
+    OUTPUT[page_czech]['czech'] = OUTPUT_CZECH[page_czech]
+
+# printer = pprint.PrettyPrinter(indent=4)
+# printer.pprint(OUTPUT)
 
 # render pages
 # create a sablona - test it
@@ -45,6 +56,5 @@ def create_pages(article_groups):
         article_list = ArticleListRender(articles_in_group)
         generated_file.write(article_list.to_html())
         generated_file.close()
-
 
 create_pages(OUTPUT)
