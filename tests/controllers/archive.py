aa5c08bfceae184ccf28c420763a104d1ff19807
Tina <tina.zhou.hui@gmail.com>
2020-06-07 17:25:33 +0200

    + archive and index tests

---
+++ tests/controllers/archive.py
@@ -0,0 +1,33 @@
+"""
+Unit test for archive page.
+"""
+import unittest
+
+from app.controllers.archive import ArchiveController
+from app.models.output_prep import create_menu
+
+
+class TestArchiveController(unittest.TestCase):
+    """
+    Unit test for archive rendering to html code.
+    """
+
+    def test_archive(self):
+        """
+        Test index on all input values.
+        """
+        all_menu = {
+            'test1': 'test1.html',
+            'test2': 'test2.html',
+            'test3': 'test3.html',
+            'test4': 'test4.html'
+        }
+        menu = create_menu(all_menu)
+
+        archive = ArchiveController(menu, all_menu)
+        output_index = archive.to_html()
+
+        self.assertIn('test1', output_index)
+        self.assertIn('test2.html', output_index)
+        self.assertIn('test3', output_index)
+        self.assertIn('test4.html', output_index)
--- tests/controllers/index.py
+++ tests/controllers/index.py
@@ -4,6 +4,7 @@ Unit test for index page.
 import unittest
 
 from app.controllers.index import IndexController
+from app.models.output_prep import create_menu, create_latest_page
 
 
 class TestIndexController(unittest.TestCase):
@@ -15,21 +16,25 @@ class TestIndexController(unittest.TestCase):
         """
         Test index on all input values.
         """
-        menu = {
+        all_menu = {
             'test1': 'test1.html',
             'test2': 'test2.html',
             'test3': 'test3.html',
+            'test4': 'test4.html'
         }
-        link = 'http//:www.test.cz'
+        menu = create_menu(all_menu)
+        link = create_latest_page(all_menu)
+
         index = IndexController(menu, link)
         output_index = index.to_html()
 
         self.assertIn('Vítejte', output_index, 'string not found')
-        self.assertIn('těší se na sud piva', output_index, 'string not found')
+        self.assertIn('těší se na sud piva', output_index, 'description string not found')
         # tenhle test me dobehne
 
         self.assertIn('test1', output_index)
         self.assertIn('test2.html', output_index)
         self.assertIn('test3', output_index)
+        self.assertNotIn('test4', output_index, 'menu not correct')
 
 
